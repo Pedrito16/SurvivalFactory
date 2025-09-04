b1906a6c8722d8d3703dfe21f9bfa13dcb6d5d00
lostada <arthurlostada@gmail.com>
2025-09-04 13:13:26 -0300

    Colliders e som de passo

--- EquinocioJogo/Assets/Scripts/PlayerFootsteps.cs
+++ EquinocioJogo/Assets/Scripts/PlayerFootsteps.cs
@@ -6,27 +6,33 @@ public class PlayerFootsteps : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip[] footstepSounds;
-    public float stepRate = 0.5f;
-    private float stepTimer;
 
+    public float walkStepRate = 0.65f;
+    public float runStepRate = 0.35f;
+
+    private float stepTimer;
     private CharacterController controller;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        stepTimer = stepRate;
+        stepTimer = walkStepRate;
     }
 
     void Update()
     {
         if (controller.isGrounded && controller.velocity.magnitude > 0.1f)
         {
+            bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+            float currentRate = isRunning ? runStepRate : walkStepRate;
+
             stepTimer -= Time.deltaTime;
 
             if (stepTimer <= 0f)
             {
                 PlayFootstep();
-                stepTimer = stepRate;
+                stepTimer = currentRate;
             }
         }
         else
@@ -39,6 +45,7 @@ public class PlayerFootsteps : MonoBehaviour
     {
         if (footstepSounds.Length > 0)
         {
+            audioSource.pitch = Random.Range(0.40f, 0.55f);
             AudioClip clip = footstepSounds[Random.Range(0, footstepSounds.Length)];
             audioSource.PlayOneShot(clip);
         }
